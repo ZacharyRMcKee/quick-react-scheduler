32c0b4201ca16a9458de95304321fca79ab5e598
ZacharyRMcKee <zmckee@hawk.iit.edu>
2019-09-30 13:19:01 -0500

    slice 2

--- src/App.js
+++ src/App.js
@@ -25,6 +25,29 @@ const schedule = {
     }
   ]
 };
+const Banner = ({ title }) => (
+  <h1>{ title }</h1>
+);
+const CourseList = ({ courses }) => (
+  <div>
+    { courses.map(course => <Course course={ course } />) }
+  </div>
+);
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+)
+  
+const Course = ({ course }) => (
+  <button>
+    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  </button>
+);
 
 const App = () =>  (
   <div>
